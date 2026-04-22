adf2c056dcf38b0dcd302f7da19a04fa93852b99
Krissh <krisshchhabra77@gmail.com>
2026-04-22 11:39:43 +0530

    feat: migrate from locat to cloud mongoDB (atlas)

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -19,7 +19,7 @@ export default function Settings() {
       {/* Header */}
       <div>
         <h1 className="text-2xl font-bold text-gray-800">
-          Settings ⚙️
+          Settings 
         </h1>
         <p className="text-gray-500">
           Manage your account
